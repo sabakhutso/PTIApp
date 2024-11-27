ffce7c65ac1be901a3aa22880c31a0b520a1b292
Nikoloz Latsabidze <baskhulukhia@gmail.com>
2024-11-28 00:46:00 +0400

    added fragment container view for main activity, core fragment for fragments, mobile navigation and minor additions.

---
+++ app/src/main/java/com/example/ptiapplicationv2/core/CorePtiFragment.kt
@@ -0,0 +1,29 @@
+package com.example.ptiapplicationv2.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+private typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+abstract class CorePtiFragment<VB : ViewBinding>(private val inflate: Inflate<VB>) : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding get() = requireNotNull(_binding)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/ptiapplicationv2/intro/IntroFragment.kt
+++ app/src/main/java/com/example/ptiapplicationv2/intro/IntroFragment.kt
@@ -1,5 +1,13 @@
 package com.example.ptiapplicationv2.intro
 
-// TODO Should create intro fragment.
-class IntroFragment {
+import android.os.Bundle
+import android.view.View
+import com.example.ptiapplicationv2.core.CorePtiFragment
+import com.example.ptiapplicationv2.databinding.FragmentIntroBinding
+
+class IntroFragment : CorePtiFragment<FragmentIntroBinding>(FragmentIntroBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO init view
+    }
 }
