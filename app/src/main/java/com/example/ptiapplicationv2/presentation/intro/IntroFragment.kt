b3eeaa16dc1da477f25bfae9ea124309a819cae5
Nikoloz Latsabidze <baskhulukhia@gmail.com>
2024-12-07 03:24:45 +0400

    added safe way of collecting flows.

--- app/src/main/java/com/example/ptiapplicationv2/presentation/intro/IntroFragment.kt
+++ app/src/main/java/com/example/ptiapplicationv2/presentation/intro/IntroFragment.kt
@@ -3,6 +3,7 @@ package com.example.ptiapplicationv2.presentation.intro
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.ptiapplicationv2.databinding.FragmentIntroBinding
@@ -18,7 +19,8 @@ class IntroFragment : CorePtiFragment<FragmentIntroBinding>(FragmentIntroBinding
         calculateDateButton.setOnClickListener { viewModel.openCalculateDatePage() }
         visitButton.setOnClickListener { viewModel.openAuthorizationPage() }
         viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.actionObserver.collect { event -> event.apply(findNavController()) }
+            viewModel.actionObserver.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+                .collect { event -> event.apply(findNavController()) }
         }
     }
 }
