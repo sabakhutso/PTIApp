551d362d4856c4969e0daec40f715046dd041c32
sabakhutso <sxucidze11@gmail.com>
2024-12-10 22:13:54 +0400

    added retrofit latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,11 +46,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+
     // Navigation
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
 }
